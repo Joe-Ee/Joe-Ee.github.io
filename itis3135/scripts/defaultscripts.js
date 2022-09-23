d04cb24bda5fa2eb6d9c60a9a7456c9f5bbc39dc
Joe_Ee <joeyskimboard@gmail.com>
2022-09-22 22:23:40 -0400

    All js scripts done

---
+++ itis3135/scripts/defaultscripts.js
@@ -0,0 +1,83 @@
+//6 js scripts for the JS page -------------------------------------------------------
+function nameMood(){
+    //save name and mood in varluables
+    var name = document.getElementById('name').value;
+    var mood = document.getElementById('moodUser').value;
+    //set the text in the h3 to the text given and the outer text too
+    document.getElementById("userName").innerHTML = "The Jovial Ocelot Welcomes You " + name + "!";
+
+    document.getElementById("myMood").innerHTML = "We're glad you are doing " + mood + "!";    
+                    }
+       //example from murach text
+function salestax(){
+        //get input
+
+    var cost = document.getElementById('cost').value;
+    var total = parseInt(cost) + .475 * parseInt(cost); //pareseint lets you go from string to integer for calcualtions
+    //set the text in the h3 to the text given and the outer text too
+
+    document.getElementById("totalTax").innerHTML = "The total after tax is $" + total;   
+                    
+                    }
+                    //From Murach Text
+function mpg(){
+    //get input
+    var miles = document.getElementById('miles').value;
+    var gallons = document.getElementById('gallons').value;
+    //set the text in the h3 to the text given and the outer text too
+    document.getElementById("answerMiles").innerHTML = "The miles per gallon is " + parseInt(miles)/parseInt(gallons) + "!";
+         
+              }
+//factorial a number
+//is basically just a while loop that goes down until the number is 1 and multipllies it by itself
+// ex 5 * 4 * 3 * 2 * 1
+function factor(numbers){
+        var ans = numbers;
+        while (numbers > 1){
+            numbers= numbers - 1;
+            ans = ans * numbers;
+                        }
+        return ans;
+                    }
+//does 5 choose 2 by taking the numbers and putting it into the equation of n!/(s!(n-s)!)
+function combinations(){
+    //variables for numbers and their facotirals
+    var total = 5;
+    var sample = 2;
+    var factorTotal = factor(5);
+    var factorSample = factor(2);
+    //total variale which is the facotiral equation with numbers
+    var total = factorTotal/(factorSample*(factor(total - sample)));
+    //output
+    document.getElementById("chosen").innerHTML = "There are " + total +" combinations for 5 choose 2.";
+                        }
+//calculate area
+function area(){
+var length = 5;
+var width = 8;
+var total = length * width;
+document.getElementById("area").innerHTML = "The area of the rectangle is " + total;
+                    }
+//calculates average score by diving the added up scores by the number of scores (4)
+function averageScore(){
+    var score1 = 90;
+    var score2 = 95;
+    var score3 = 83;
+    var score4 = 99;
+    var amount = 4;
+    var total = score1 + score2 + score3 + score4;
+    total = total/amount;
+    document.getElementById("averageScores").innerHTML = "The average between " +score1 + ", " + score2  + ", " + score3 + ", " + score4 + " is " +total;
+
+}
+//----------------------------------------------------------------------------------------------------------------
+    //make a new date object                    
+    const d = new Date();
+    document.getElementById("time").innerHTML = d.toLocaleTimeString('en-US');
+    //this line is getting the date name weekday name by going into teh array of weekdays and then looking for the corresponding date name
+    document.getElementById("date").innerHTML = d.toLocaleDateString('en-US', { weekday: 'long'});
+    document.getElementById("day").innerHTML = d.getDay();
+    //get the long month from teh array
+    document.getElementById("month").innerHTML = d.toLocaleDateString('default', {month: 'long'});
+    document.getElementById("year").innerHTML = d.getFullYear();
+
