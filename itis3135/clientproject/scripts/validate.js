bb00b7aa7a0a6ea01ab9b876767a567fa6f7ca20
Joe_Ee <joeyskimboard@gmail.com>
2022-11-28 18:46:30 -0500

    validatejs to client

---
+++ itis3135/clientproject/scripts/validate.js
@@ -0,0 +1,2 @@
+document.getElementById("validation_link_html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
+document.getElementById("validation_link_css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
