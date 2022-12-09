950e20b58934ca259b893af4f578c4a9b9406058
Joe_Ee <joeyskimboard@gmail.com>
2022-12-08 23:26:12 -0500

    WORK

---
+++ itis3135/clientproject/scripts/carosuel.js
@@ -0,0 +1,12 @@
+$(function(){
+    
+        $(".box").ig3js({
+    
+        manifest: [ 
+        {src:"include2.png", id:"image1"},  
+        {src:"include1.png", id:"image2"}
+        ],
+        imagePath: 'images/',
+        
+        });
+        });
